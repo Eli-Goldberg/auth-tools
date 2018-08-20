9d43f1dfe4cdaf1a536421641ca928a968dc6dfc
Eli Goldberg <eli.b.goldberg@gmail.com>
2018-08-20 11:08:20 +0300

    change package name to @compit/auth-tools

--- test/token.test.js
+++ test/token.test.js
@@ -18,14 +18,14 @@ describe('token', () => {
     expect(catched).to.exist;
   });
   it('should be able to verify a signed token', async () => {
-    const testPayload = { msg: '123' }
+    const testPayload = { msg: '123' };
     const token = await signToken({ payload: testPayload, secret, expiresIn: '7d' });
 
     const decoded = await verifyToken({ token, secret });
     expect(decoded).to.contain(testPayload);
   });
   it('should not be able to verify an expired token', async () => {
-    const testPayload = { msg: '123' }
+    const testPayload = { msg: '123' };
     // Set the expiry for 50 ms
     const token = await signToken({ payload: testPayload, secret, expiresIn: '50' });
 
